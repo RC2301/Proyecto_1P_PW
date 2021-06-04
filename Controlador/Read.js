b18304a11062c37437744536bf4417633f5d80a4
RC2301 <richardcamacho66@hotmail,com>
2021-06-03 22:14:40 -0500

    Vector de arreglos con los años.

--- Controlador/Read.js
+++ Controlador/Read.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const http = require('http');
 const colors = require("colors")
 let datosCSV = [];
+let resultado = [];
 const read = (path, cod, year, guardar) => {
     let n = 0;
     let pais;
@@ -42,4 +43,89 @@ const read = (path, cod, year, guardar) => {
                 console.log("Se guardaron exitosamente los datos.".bgGreen);
             }
         });
+}
+const arreglar = (cod, year) => {
+    var arr = datosCSV.map(item => {
+        return {
+            nombre_ciudad: item[0],
+            codigo_ciudad: item[1],
+            year: {
+                '1960': item[4],
+                '1961': item[5],
+                '1962': item[6],
+                '1963': item[7],
+                '1964': item[8],
+                '1965': item[9],
+                '1966': item[10],
+                '1967': item[11],
+                '1968': item[12],
+                '1969': item[13],
+                '1970': item[14],
+                '1971': item[15],
+                '1972': item[16],
+                '1973': item[17],
+                '1974': item[18],
+                '1975': item[19],
+                '1976': item[20],
+                '1977': item[21],
+                '1978': item[22],
+                '1979': item[23],
+                '1980': item[24],
+                '1981': item[25],
+                '1982': item[26],
+                '1983': item[27],
+                '1984': item[28],
+                '1985': item[29],
+                '1986': item[30],
+                '1987': item[31],
+                '1988': item[32],
+                '1989': item[33],
+                '1990': item[34],
+                '1991': item[35],
+                '1992': item[36],
+                '1993': item[37],
+                '1994': item[38],
+                '1995': item[39],
+                '1996': item[40],
+                '1997': item[41],
+                '1998': item[42],
+                '1999': item[43],
+                '2000': item[44],
+                '2001': item[45],
+                '2002': item[46],
+                '2003': item[47],
+                '2004': item[48],
+                '2005': item[49],
+                '2006': item[50],
+                '2007': item[51],
+                '2008': item[52],
+                '2009': item[53],
+                '2010': item[54],
+                '2011': item[55],
+                '2012': item[56],
+                '2013': item[57],
+                '2014': item[58],
+                '2015': item[59],
+                '2016': item[60],
+                '2017': item[61],
+                '2018': item[62],
+                '2019': item[63],
+                '2020': item[64]
+            }
+
+        };
+    });
+    let countryC = require("../Datos/ISO-3166-ALPHA-3.json");
+    let arreglado = [];
+    for (const i in arr) {
+        for (const key in countryC) {
+            if (arr[i].codigo_ciudad === countryC[key].countryCode) {
+                arreglado.push(arr[i]);
+            }
+        }
+    }
+    calcularMedia(arreglado, cod, year);
+    let top = topCinco(arreglado, year);
+    resultado.push({ top5: top });
+    return arreglado;
 }
